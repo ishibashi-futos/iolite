e3fefe9d5290742a4f131fe88b585439529ce560
ishibshi.futos <ishibshi.futos@outlook.com>
2020-04-22 17:36:36 +0900

    answerを追加

--- public/renderer.js
+++ public/renderer.js
@@ -3,13 +3,21 @@
 window.onload = () => {
   const peer = new Peer();
   const video = document.querySelector("video");
-  navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((mediaStrean) => {
-    video.srcObject = mediaStrean;
-    const call = peer.call("iolite", mediaStrean);
+  navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((localStream) => {
+    video.srcObject = localStream;
+    const call = peer.call("iolite", localStream);
     call.on("stream", (remoteStream) => {
       const anotherMedia = document.createElement("video");
       anotherMedia.srcObject = remoteStream;
       document.querySelector("#remoteMedia").appendChild(anotherMedia);
+    });
+    peer.on("call", (call) => {
+      call.ansewer(localStream);
+      call.on("stream", (remoteStream) => {
+        const anotherMedia = document.createElement("video");
+        anotherMedia.srcObject = remoteStream;
+        document.querySelector("#remoteMedia").appendChild(anotherMedia);
+      });
     });
   }).catch((err) => {
     alert(err);
